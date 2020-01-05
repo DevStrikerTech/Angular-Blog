401d362aabf5ff5910fbdb336433f646cdb5241b
KingCobra2018 <kcobra557@gmail.com>
2020-01-05 01:19:25 +0000

    configure routing for new component called add-blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import {AddBlogComponent} from './add-blog/add-blog.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'add-blog', component: AddBlogComponent }
 ];
 
 @NgModule({
