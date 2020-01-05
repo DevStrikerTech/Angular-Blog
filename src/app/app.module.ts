1228ba42a6a63dc17276ce5c2926253c31a0c440
KingCobra2018 <kcobra557@gmail.com>
2020-01-05 00:39:35 +0000

    removed boiler Angular html template and added new home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
